9efe7be6fecd547c4f744ca44be3c7cbd81920b9
Adrian <adrianhthompson@gmail.com>
2018-10-05 15:18:16 +0200

    changed from button to to TouchableOpacity for easier styling

--- src/components/createEvent/dateTimePicker.js
+++ src/components/createEvent/dateTimePicker.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
-import { Button, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 import DateTimePicker from 'react-native-modal-datetime-picker';
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    paddingTop: 12.5,
+    backgroundColor: '#AE52D4',
+    height: 45,
+    margin: 15,
+    width: 300,
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 20
+  }
+});
+
 
 export default class DateTimePickerTester extends Component {
   state = {
@@ -19,9 +35,10 @@ export default class DateTimePickerTester extends Component {
   render () {
     return (
       <View>
-        <TouchableOpacity>
-          <Button title="PICCA THE DATE" onPress={this.showDateTimePicker}
-          color="#AE52D4" />
+        <TouchableOpacity onPress={this.showDateTimePicker} style={styles.button}>
+          <Text style={styles.text}>
+            PICCA THE DATE
+          </Text>
         </TouchableOpacity>
         <DateTimePicker
           isVisible={this.state.isDateTimePickerVisible}
