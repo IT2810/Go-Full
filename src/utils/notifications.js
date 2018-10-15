bef196e08653a5f548d9f2175f00cd77c40b1519
Aslak Hollund <aslakhol@gmail.com>
2018-10-15 17:46:57 +0200

    add usage comments

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,5 +1,16 @@
 import { Notifications } from 'expo';
 
+// Usage
+// import notificationUtil from './notifications'
+// call notificationUtil.sendNotificationAsync(title, body, channelId, date)
+// title and body are strings.
+// channelId is one of these three strings 'test', 'mission-critical' and 'nudge'.
+// date is a date object that decides when the notification is sent.
+
+// if you want to have ability to cancel a notification you need
+// to store the id that sendNotificationAsync returns
+// to do this the function must be awaited.
+
 const notificationUtil = {
   async sendNotificationAsync(title, body, channelId, date) {
     const notificationObject = {
