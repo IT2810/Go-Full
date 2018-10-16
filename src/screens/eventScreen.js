2e0851ce0f00abc816a5c50c75a676ed30fb3f61
Lars Gjelstad <largje@dhcp-10-24-3-36.wlan.ntnu.no>
2018-10-16 12:43:14 +0200

    Added components, eventScreen

--- src/screens/eventScreen.js
+++ src/screens/eventScreen.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {View, TouchableOpacity, Text} from 'react-native';
+import StyleSheet from 'react-native-material-ui';
 
+
+const styles = ({        // Styling for different components
+    eventTitle: {
+        width: 132,
+        height: 24,
+        fontFamily: Roboto,
+        fontStyle: Medium,
+        fontSize: 20,
+        lineHeight: 23,
+        alignItems: Left,
+    },
+
+    container: {                           // container - buttons
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+
+    addBeer: {
+        width: '25%',
+        backgroundColor: "#38006B"
+
+    },
+
+    addDrink: {
+        width: '25%',
+        backgroundColor: "#38006B"
+
+    },
+
+    addWine: {
+        width: '25%',
+        backgroundColor: "#38006B"
+
+    },
+})
 
 const EventScreen = (props) => {
   const { navigation } = props;
   return (
     <View>
-      <Text>
-        {navigation.getParam('title')}
-      </Text>
+        <Text>
+            {navigation.getParam('title')}
+        </Text>
+
+        <View style={styles.container}>
+            <View style={styles.addBeer}>
+                <TouchableOpacity>
+                    icon = {<Icon name={"fa-beer"} onAction/>}
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.addDrink}>
+                <TouchableOpacity>
+                    icon = {<Icon name={"glass-martini"}/>}
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.addWine}>
+                <TouchableOpacity>
+                    icon ={<Icon name={"wine-glass"}/>}
+
+                </TouchableOpacity>
+            </View>
+        </View>
     </View>
   );
 };
-
 
 export default EventScreen;
