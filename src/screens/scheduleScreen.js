4c4920182aa005915490f1773843eebf681426af
Adrian <adrianhthompson@gmail.com>
2018-10-16 08:32:36 +0200

    Added navigation button to createEvent screen

--- src/screens/scheduleScreen.js
+++ src/screens/scheduleScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+import { ActionButton } from 'react-native-material-ui';
 import Schedule from '../components/Schedule';
 import { AppContext } from '../components/AppProvider';
+
 
 const schedulescreen = (props) => {
   const { navigation } = props;
@@ -12,6 +14,15 @@ const schedulescreen = (props) => {
           <Schedule navigation={navigation} appState={appState} />
         )}
       </AppContext.Consumer>
+      <View style={{
+        position: 'fixed', right: 0, top: 430,
+      }}
+      >
+        <ActionButton
+          buttonColor="rbga(156,77,204,1)"
+          onPress={() => navigation.navigate('createEvent')}
+        />
+      </View>
     </View>);
 };
 
