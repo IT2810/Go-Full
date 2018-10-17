9bbf69d5bb482c1bd65357a8e6f4a4a870af0960
Aslak Hollund <aslakhol@gmail.com>
2018-10-17 12:46:50 +0200

    mock asyncStorage

--- src/tests/AppProviderTests.test.js
+++ src/tests/AppProviderTests.test.js
@@ -1,4 +1,24 @@
-test('1 = 1', () => {
-  const one = 1;
-  expect(one).toEqual(1);
-})
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AppProvider from '../components/AppProvider';
+
+// Mocking AsyncStorage
+const mockFoo = {
+  keyOne: 'valueOne',
+  keyTwo: 'valueTwo',
+};
+
+jest.mock('react-native', () => ({
+  AsyncStorage: {
+    setItem: jest.fn((key, value) => new Promise((resolve) => {
+      mockFoo[key] = value;
+      resolve(value);
+    })),
+    getItem: jest.fn(key => new Promise((resolve) => {
+      resolve(mockFoo[key]);
+    })),
+  },
+}));
+
+
+jest.unmock('react-native');
